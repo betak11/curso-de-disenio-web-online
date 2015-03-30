8ce99decd64b52c597441d871b4dd07d801281b1
jalgaba <jalgaba@colef.mx>
2015-03-29 21:48:36 -0700

    TAREA KEVIN
    ARREGLOS Y FUNCIONES

---
+++ HTML5 y conceptos básicos/TAREA KEVIN ARREGLOS Y FUNCIONES.js
@@ -0,0 +1,41 @@
+//TAREA
+//:
+//Crear las siguientes funciones:
+
+//* Funcion con nombre que no reciba ningun parametro y que no regresa nada:
+function mifuncion(){}
+
+//* Funcion anónima que no reciba ningun parametro y que no regresa nada:
+
+//* Almacena una funcion anonima en una variable llamada "mifuncion"
+
+//* Funcion con nombre que reciba un parametro y no regrese nada
+
+//* Funcion con nombre que reciba un parametro y regrese ese mismo paramtetro
+
+//* Funcion con nombre que reciba 2 parametros y regrese solo el primer paramtro
+
+//* Funcion con nombre que reciba 2 paramtros y regrese la suma de los 2 paramtros
+
+//Arreglo con 3 numeros almacenado en una variable llamada "miarreglo"
+
+//del arreglo anterior, obten cuantos elementos tiene
+
+// del mismo arreglo, saca el segundo elemento
+
+// de un arreglo que NO SABES CUANTOS ELEMENTOS TIENE saca el ULTIMO ELEMENTO
+
+// Arreglo con un numero, un string, un arreglo (con 3 numeros), un valor booleando y una funcion
+
+// Del arreglo anterior, accesa el segundo elemento del arreglo dentro del arreglo
+
+// Crea un objeto de la mantera vieja y de la manera nueva
+
+// Crea un objeto de la manera vieja y la manera nueva, y ponle unas 3 propiedades a cada uno, tanto de la vieja como la nueva manera
+
+// Crea un objeto con dos propiedades:
+//* La primera se llamara adrian y su valor sera la edad de adrian, la seguna sera tu kevin shalom (con espacio), y el valor sera tu edad
+
+//De la variable anterior, accesa la propiedad de adrian, y luego la propiedad de kevin shalom (tendras que usar los corchetes)
+
+//Crea 
